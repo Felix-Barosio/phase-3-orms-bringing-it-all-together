db050edb15d2f3ee2fab55be431d257f781d6b06
Felix <barosiofelix@gmail.com>
2022-10-30 12:24:48 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -118,10 +118,10 @@ class Dog
     def update
 
         sql = <<-SQL
-        UPDATE dogs SET id = ?, name = ?, breed = ?
+        UPDATE dogs SET name = ?, breed = ? WHERE id = ?
         SQL
 
-        DB[:conn].execute(sql, self.id, self.name, self.breed)
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
 
     end
 
